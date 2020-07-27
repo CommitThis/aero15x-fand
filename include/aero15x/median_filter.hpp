1d1175d1673734a1faf8dc0b1e863eac6d06edfa
G Davey <gdavey@committhis.co.uk>
2020-07-27 21:18:22 +0100

    if constexpr, minor changes

--- include/aero15x/median_filter.hpp
+++ include/aero15x/median_filter.hpp
@@ -17,16 +17,17 @@ public:
 
     auto filter(double value) -> double
     {
-        static constexpr auto half_point = Size / 2;
-
+        /* Rotate buffer */
         for (auto ii = Size; ii != 0; --ii) {
             m_buffer[ii] = m_buffer[ii-1];
         }
         m_buffer[0] = value;
-        auto sorted = std::array<double, Size>{};
-        sorted = m_buffer;
+
+        auto sorted = std::array<double, Size>{m_buffer};
         std::sort(sorted.begin(), sorted.end());
-        if (Size % 2 == 0) {
+
+        static constexpr auto half_point = Size / 2;
+        if constexpr (Size % 2 == 0) {
             return (sorted[half_point] + sorted[half_point - 1]) / 2.0;
         }
         else {
